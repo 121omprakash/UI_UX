8fb9ccd40d043862768326b06b16b2ea1b74bcf0
Om Prakash <132379517+121omprakash@users.noreply.github.com>
2024-10-09 13:20:45 +0000

    updated login-logout

--- Pages/Dashboard/script.js
+++ Pages/Dashboard/script.js
@@ -72,12 +72,16 @@ darkModeToggle.addEventListener('click', () => {
     }
 });
 
-
+if(localStorage.getItem('isLoggedIn')!==true)
+{
+    window.location.href = 'https://121omprakash.github.io/UI_UX/';
+}
 // Sign Out Button
 const signOutBtn = document.getElementById('signOutBtn');
 signOutBtn.addEventListener('click', () => {
     // Redirect to the login page when signing out
-    window.location.href = 'https://121omprakash.github.io/UI_UX/'; // Ensure this path is correct based on your file structure
+    localStorage.removeItem('isLoggedIn');
+    window.location.href = 'https://121omprakash.github.io/UI_UX/';
 });
 
 
